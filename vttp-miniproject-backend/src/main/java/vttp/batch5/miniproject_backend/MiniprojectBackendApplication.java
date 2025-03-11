df04f0bb7b195edfffa180033ce2f77abc9f1e1f
Varshini <varshinip002@gmail.com>
2025-03-11 13:54:33 +0800

    initialised project

---
+++ vttp-miniproject-backend/src/main/java/vttp/batch5/miniproject_backend/MiniprojectBackendApplication.java
@@ -0,0 +1,13 @@
+package vttp.batch5.miniproject_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojectBackendApplication.class, args);
+	}
+
+}
